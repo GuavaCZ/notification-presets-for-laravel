1fe7b77155c6f8406a25d7bc75124fd4da73ffb5
Lukas Frey <mail@lukasfrey.cz>
2025-01-21 09:40:59 +0100

    chore: basic setup

---
+++ src/Contracts/Subscribable.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Guava\NotificationPresetsForLaravel\Contracts;
+
+interface Subscribable
+{
+
+}
---
+++ src/Models/NotificationPreset.php
@@ -0,0 +1,8 @@
+<?php
+
+namespace Guava\NotificationPresetsForLaravel\Models;
+
+class NotificationPreset
+{
+
+}
