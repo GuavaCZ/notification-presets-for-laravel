cd07057210c0859076b496ea1bed7fde81170a4f
stepanmocjak <stepan.mocjak@guava.cz>
2025-01-21 09:50:25 +0100

    feat: Subscribable interface

--- src/Contracts/Subscribable.php
+++ src/Contracts/Subscribable.php
@@ -4,5 +4,11 @@ namespace Guava\NotificationPresetsForLaravel\Contracts;
 
 interface Subscribable
 {
+    public function id(): string;
 
+    public function name(): string;
+
+    public function subscribableChannels(): array;
+
+    public function defaultSubscriptionPreferences(): array;
 }
